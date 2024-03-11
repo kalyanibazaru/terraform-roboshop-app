c19a884203bc1bd10c3ee55084de0780c9b69637
Kalyanibazaru <bazaru.kalyani@gmail.com>
2024-03-11 21:45:55 +0530

    terraform-roboshop

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "common_tags" {
+#   default = {
+#     Project     = "roboshop"
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
+}
+
+variable "tags" {
+    
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  #default = "bkdevops.online"
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "component_sg_id" {
+  
+}
+variable "private_subnet_ids" {
+  
+}
+variable "iam_instance_profile" {
+  
+}
+variable "app_alb_listener_arn" {
+  
+}
+variable "rule_priority" {
+  
+}
